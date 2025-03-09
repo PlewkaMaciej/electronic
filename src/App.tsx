cb766e2af37b81e7879003b0afcfe13ab9b07991
unknown <kamilplewka12a@wp.pl>
2025-03-09 19:55:37 +0100

    Add RectangleaD

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import Announcement from "../component/Items/Announcement";
 import CarouselWrapper from "./Example";
-
+import RectangleAd from "../component/Items/RectangleAd"
 function App() {
   return (
     <>
@@ -20,6 +20,18 @@ function App() {
             }
           />
           <Route path="/carousel" element={<CarouselWrapper />} />
+          <Route
+            path="/rectanglead"
+            element={
+              <RectangleAd
+                imageSrc="https://www.example.com/ad-image.jpg"
+                name="Przykładowa nazwa ogłoszenia Przykładowa nazwa ogłoszenia Przykładowa1"
+                dateAdded="2025-03-09"
+                specification="Intel i7, 16GB RAM, 1TB SSD"
+                price="3500"
+              />
+            }
+          /> {/* Poprawiona ścieżka */}
         </Route>
       </Routes>
     </>
