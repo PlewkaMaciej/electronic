b1493c1823f510e3f2b6bc41c98f411351879793
Plewka Maciej <plewka-maciej@wp.pl>
2025-03-14 18:44:47 +0100

    change design of rectanglead

--- component/Items/RectangleAd.tsx
+++ component/Items/RectangleAd.tsx
@@ -8,28 +8,36 @@ interface RectangleAdProps {
   price: string;
 }
 
-function RectangleAd({ imageSrc, name, dateAdded, specification, price }: RectangleAdProps) {
+function RectangleAd({
+  imageSrc,
+  name,
+  dateAdded,
+  specification,
+  price,
+}: RectangleAdProps) {
   return (
-    <div className="flex items-start w-full max-w-[800px] h-[150px] border border-gray-500 rounded-lg shadow-md p-4 bg-[#F0F1EC] overflow-hidden">
+    <div className="flex items-center w-full max-w-[850px] h-[160px] border border-gray-300 rounded-xl shadow-lg p-4 bg-white overflow-hidden">
       {/* Kolumna 1 - Zdjęcie */}
-      <div className="w-[120px] h-[120px] overflow-hidden rounded-md border border-gray-400 shadow-sm">
+      <div className="w-[130px] h-[130px] overflow-hidden rounded-lg border border-gray-300 shadow-sm">
         <img src={imageSrc} alt={name} className="object-cover w-full h-full" />
       </div>
 
       {/* Kolumna 2 - Nazwa i Data */}
-      <div className="flex-2 px-4 max-w-full h-[120px] flex flex-col justify-between">
-        <p className="text-sm md:text-base font-bold text-gray-700">{name}</p> {/* Smaller font size */}
-        <p className="text-xs text-gray-500">{dateAdded}</p> {/* Align to the bottom */}
+      <div className="flex-2 px-4 flex flex-col justify-between h-full">
+        <p className="text-base md:text-lg font-semibold text-gray-800">
+          {name}
+        </p>
+        <p className="text-xs text-gray-500">{dateAdded}</p>
       </div>
 
       {/* Kolumna 3 - Specyfikacja */}
-      <div className="flex-1 px-4 max-w-full overflow-y-auto">
-        <p className="text-sm text-gray-600">Specyfikacja:</p>
-        <p className="text-sm text-gray-600">{specification}</p>
+      <div className="flex-1 px-4 max-w-full overflow-hidden">
+        <p className="text-sm text-gray-700 font-medium">Specyfikacja:</p>
+        <p className="text-sm text-gray-600 truncate">{specification}</p>
       </div>
 
       {/* Kolumna 4 - Cena */}
-      <div className="text-lg font-bold text-gray-900 px-4">
+      <div className="text-xl font-bold text-gray-900 px-4 bg-gray-100 rounded-lg p-2 shadow-md">
         {price} zł
       </div>
     </div>
