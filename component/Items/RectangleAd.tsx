9bc7cee6ff7171281fd576cd12edc8c0e27e51ca
Plewka Maciej <plewka-maciej@wp.pl>
2025-03-16 17:47:08 +0100

    add second version of carousel

--- component/Items/RectangleAd.tsx
+++ component/Items/RectangleAd.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 interface RectangleAdProps {
   imageSrc: string;
   name: string;
@@ -17,12 +15,10 @@ function RectangleAd({
 }: RectangleAdProps) {
   return (
     <div className="flex items-center w-full max-w-[850px] h-[160px] border border-gray-300 rounded-xl shadow-lg p-4 bg-white overflow-hidden">
-      {/* Kolumna 1 - Zdjęcie */}
       <div className="w-[130px] h-[130px] overflow-hidden rounded-lg border border-gray-300 shadow-sm">
         <img src={imageSrc} alt={name} className="object-cover w-full h-full" />
       </div>
 
-      {/* Kolumna 2 - Nazwa i Data */}
       <div className="flex-2 px-4 flex flex-col justify-between h-full">
         <p className="text-base md:text-lg font-semibold text-gray-800">
           {name}
@@ -30,13 +26,11 @@ function RectangleAd({
         <p className="text-xs text-gray-500">{dateAdded}</p>
       </div>
 
-      {/* Kolumna 3 - Specyfikacja */}
       <div className="flex-1 px-4 max-w-full overflow-hidden">
         <p className="text-sm text-gray-700 font-medium">Specyfikacja:</p>
         <p className="text-sm text-gray-600 truncate">{specification}</p>
       </div>
 
-      {/* Kolumna 4 - Cena */}
       <div className="text-xl font-bold text-gray-900 px-4 bg-gray-100 rounded-lg p-2 shadow-md">
         {price} zł
       </div>
