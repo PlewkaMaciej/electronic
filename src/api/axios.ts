1d84ed46d0d36a81047fc3761cf327b5876f4203
Plewka Maciej <plewka-maciej@wp.pl>
2025-06-07 22:06:51 +0200

    add redux

---
+++ src/api/axios.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const baseURL = import.meta.env.VITE_API_URL || "http://localhost:4000/api";
+
+const api = axios.create({ baseURL });
+
+const token = localStorage.getItem("accessToken");
+if (token) {
+  api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+}
+
+export default api;
